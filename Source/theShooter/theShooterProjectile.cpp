f9acb0d4ed2d01134811825aa5ed3490a605e116
David Garcia <Anubis.XII@protonmail.com>
2025-01-25 14:58:55 -0600

    Created decal, updated projectile script. Added random color to projectile on hit

--- Source/theShooter/theShooterProjectile.cpp
+++ Source/theShooter/theShooterProjectile.cpp
@@ -4,22 +4,35 @@
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Components/SphereComponent.h"
 
+#include "Kismet/KismetMathLibrary.h"
+#include "Components/DecalComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+//#include "PerlinProcTerrain.h"
+
 AtheShooterProjectile::AtheShooterProjectile() 
 {
-	// Use a sphere as a simple collision representation
+	// Creates the sphere collision component
 	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
 	CollisionComp->InitSphereRadius(5.0f);
+
+
+	// Puts the collision componenton the projectile
 	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
-	CollisionComp->OnComponentHit.AddDynamic(this, &AtheShooterProjectile::OnHit);		// set up a notification for when this component hits something blocking
+
+	// detects if the component is hit by something
+	CollisionComp->OnComponentHit.AddDynamic(this, &AtheShooterProjectile::OnHit);
 
 	// Players can't walk on it
 	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
 	CollisionComp->CanCharacterStepUpOn = ECB_No;
 
-	// Set as root component
+	// Sets the root component
 	RootComponent = CollisionComp;
 
-	// Use a ProjectileMovementComponent to govern this projectile's movement
+	// Creates the attributes of the projectile
 	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
 	ProjectileMovement->UpdatedComponent = CollisionComp;
 	ProjectileMovement->InitialSpeed = 3000.f;
@@ -27,17 +40,76 @@ AtheShooterProjectile::AtheShooterProjectile()
 	ProjectileMovement->bRotationFollowsVelocity = true;
 	ProjectileMovement->bShouldBounce = true;
 
-	// Die after 3 seconds by default
-	InitialLifeSpan = 3.0f;
+	// Lifespan of the projectile
+	InitialLifeSpan = 4.0f;
+
+	// Sets up the ball mesh component and the root comp of the ball
+	ballMesh = CreateDefaultSubobject<UStaticMeshComponent>("Ball Mesh");
+	RootComponent = CollisionComp;
+	ballMesh->SetupAttachment(CollisionComp);
+
 }
+
+void AtheShooterProjectile::BeginPlay()
+{
+	// Soon as the game starts 
+	Super::BeginPlay();
+
+	//Picks a random color and assigns the coloe to randColor
+	randColor = FLinearColor(UKismetMathLibrary::RandomFloatInRange(0.f, 1.f), UKismetMathLibrary::RandomFloatInRange(0.f, 1.f), UKismetMathLibrary::RandomFloatInRange(0.f, 1.f), 1.f);
+
+	// Creates the matieral in the editor
+	dmiMat = UMaterialInstanceDynamic::Create(projMat, this);
+
+	// Sets the matieral to the randColor color
+	if (dmiMat)
+	{
+		ballMesh->SetMaterial(0, dmiMat);
+		dmiMat->SetVectorParameterValue("ProjColor", randColor);
+	}
+
+	// if the color is not assigned throw off an error message
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("SMI MAT not applied in details panel"));
+		return;
+	}
+	
+}
+
 
 void AtheShooterProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	// Only add impulse and destroy projectile if we hit a physics
+	// Only add impulse and destroy projectile if it is hit 
 	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
 	{
 		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
 
 		Destroy();
+	}
+
+	if (OtherActor != nullptr)
+	{
+		// Chooses a random value for the color within a range of 0.f and 1.f
+		float ranNumX = UKismetMathLibrary::RandomFloatInRange(0.f, 1.f);
+		float ranNumY = UKismetMathLibrary::RandomFloatInRange(0.f, 1.f);
+		float ranNumZ = UKismetMathLibrary::RandomFloatInRange(0.f, 1.f);
+
+		// chooses which image to use in the 2x2 decal
+		float frameNum = UKismetMathLibrary::RandomFloatInRange(0.f, 3.f);
+
+		// Sets the decal based on the values created
+		FVector4 localRandColor = FVector4(ranNumX, ranNumY, ranNumZ, 1.f);
+
+		//
+		auto Decal = UGameplayStatics::SpawnDecalAtLocation(GetWorld(), baseMat, FVector(UKismetMathLibrary::RandomFloatInRange(20.f, 40.f)), Hit.Location, Hit.Normal.Rotation(), 0.f);
+		auto MatInstance = Decal->CreateDynamicMaterialInstance();
+
+		MatInstance->SetVectorParameterValue("Color", randColor);
+		MatInstance->SetScalarParameterValue("Frame", frameNum);
+
+		Decal->SetLifeSpan(5.0f);
+
+		
 	}
 }
--- Source/theShooter/theShooterProjectile.h
+++ Source/theShooter/theShooterProjectile.h
@@ -8,6 +8,7 @@
 
 class USphereComponent;
 class UProjectileMovementComponent;
+class UNiagaraSystem;
 
 UCLASS(config=Game)
 class AtheShooterProjectile : public AActor
@@ -22,16 +23,58 @@ class AtheShooterProjectile : public AActor
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	UProjectileMovementComponent* ProjectileMovement;
 
+
+
+	// CREATES A componenet for the BP called ballMesh and is able to be edited in the components editor in unreal
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* ballMesh;
+
+
+	// Create the BASE MATERIAL OF THE COMP
+	UPROPERTY(EditAnywhere)
+		UMaterial* baseMat;
+
+	// Creates a variable called randColor
+	UPROPERTY(EditAnywhere)
+		FLinearColor randColor;
+
+
+	// Adds the ability to add different materials to the projectile
+	UPROPERTY(EditAnywhere)
+		UMaterialInterface* projMat;
+
+	// Changes the material to a color 
+	UPROPERTY(EditAnywhere)
+		UMaterialInstanceDynamic* dmiMat;
+
+
+
+	UPROPERTY(EditAnywhere)
+		UNiagaraSystem* colorP;
+
+
+
 public:
 	AtheShooterProjectile();
 
-	/** called when projectile hits something */
-	UFUNCTION()
-	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
-	/** Returns CollisionComp subobject **/
+protected:
+	virtual void BeginPlay();
+
+
+public:
+
+	// called when projectile hits something and is a function
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+
+
+	// Returns CollisionComp subobject
 	USphereComponent* GetCollisionComp() const { return CollisionComp; }
-	/** Returns ProjectileMovement subobject **/
+
+
+	// Returns ProjectileMovement subobject
 	UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
 };
 
